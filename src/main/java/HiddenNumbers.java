f0ca99717d77fefa14e5a79505d6f757ea6f77a9
Paula Bonastre <bonastrep@gmail.com>
2018-04-28 12:27:41 +0200

    program finish

--- src/main/java/HiddenNumbers.java
+++ src/main/java/HiddenNumbers.java
@@ -1,15 +1,18 @@
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.*;
+import java.util.function.Consumer;
 
 import static java.lang.Integer.parseInt;
 
 public class HiddenNumbers {
 
-    private static final String INFILENAME = "inputChallenge.txt";
-    private static String OUTFILENAME = "outputChallenge.txt";
+    private static final String INFILENAME = "testInput.txt";
+    private static String OUTFILENAME = "testoutput.txt";
     public static BufferedReader reader = null;
     private static PrintWriter out;
     private static int AMOUNT_CASES = 0;
+    private static int BASE = 0;
     HashMap<String, ArrayList> perputationPosibilities = new HashMap<String, ArrayList>();
 
 
@@ -18,59 +21,30 @@ public class HiddenNumbers {
             File file = new File(INFILENAME);
             StringBuffer contents = new StringBuffer();
 
-            reader = new BufferedReader(new FileReader(file));
-            retrieveInput();
-            reader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+            out = new PrintWriter(OUTFILENAME);
 
-
-    private static void retrieveInput() {
-        File file = new File(INFILENAME);
-        StringBuilder contents = new StringBuilder();
-        try {
             reader = new BufferedReader(new FileReader(file));
             obtainCaseAmount();
-
             for (int i = 0; i < AMOUNT_CASES; i++) {
-                permutationPosibilities();
+                double hiddenNum = obtainHiddenNumber();
+                printHiddenNumber(i, hiddenNum, out);
             }
+
+            reader.close();
+            out.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-    }
-
-    private static void permutationPosibilities() {
-        try {
-            String sCurrentLine = reader.readLine();
-            Integer[] values = new Integer[sCurrentLine.length()];
-
-            for (int i = 0; i < sCurrentLine.length(); i++) {
-                values[i] = i;
-            }
-            Set<Integer[]> options = getPermutationsRecursive(values);
-            removeInitializeZero(options);
-
-            //perputationPosibilities.add();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void removeInitializeZero(Set<Integer[]> options) {
-        Iterator<Integer[]> iterator = options.iterator();
-        while(iterator.hasNext()) {
-            Integer[] setElement = iterator.next();
-            if(setElement.toString().charAt(0) == 0) {
-                iterator.remove();
-            }
-        }
+    private static void printHiddenNumber(int index,double hiddenNum, PrintWriter out) {
+        DecimalFormat df = new DecimalFormat("##0");
+        out.print("Case #" + index + ": ");
+        out.print(df.format(hiddenNum));
+        out.println();
     }
-
 
     private static void obtainCaseAmount() {
         try {
@@ -84,34 +58,87 @@ public class HiddenNumbers {
 
     }
 
-    public static Set<Integer[]> getPermutationsRecursive(Integer[] num) {
-        if (num == null)
-            return null;
+    private static double  obtainHiddenNumber() {
+        HashSet<String>  possibilitiesPermu = permutationPossibilities();
+        double hiddenNum = calculateDecimalValue(possibilitiesPermu);
+         return hiddenNum;
+   }
 
-        Set<Integer[]> perms = new HashSet<>();
-
-        //base case
-        if (num.length == 0) {
-            perms.add(new Integer[0]);
-            return perms;
+    private static double calculateDecimalValue(HashSet<String> permuPossibilities) {
+        Iterator posIterator = permuPossibilities.iterator();
+        double decimal = 0;
+        double maxValue = 0;
+        double minValue = Double.MAX_VALUE;
+        while (posIterator.hasNext()){
+            String chainValue = (String)posIterator.next();
+            decimal = decimalValue(chainValue);
+            if(decimal > maxValue) maxValue = decimal;
+            if(decimal < minValue) minValue = decimal;
         }
-
-        //shave off first int then get sub perms on remaining ints.
-        //...then insert the first into each position of each sub perm..recurse
-        int first = num[0];
-        Integer[] remainder = Arrays.copyOfRange(num, 1, num.length);
-        Set<Integer[]> subPerms = getPermutationsRecursive(remainder);
-        for (Integer[] subPerm : subPerms) {
-            for (int i = 0; i <= subPerm.length; ++i) { // '<='   IMPORTANT !!!
-                Integer[] newPerm = Arrays.copyOf(subPerm, subPerm.length + 1);
-                for (int j = newPerm.length - 1; j > i; --j)
-                    newPerm[j] = newPerm[j - 1];
-                newPerm[i] = first;
-                perms.add(newPerm);
-            }
-        }
-
-        return perms;
+        return (maxValue-minValue);
     }
 
-}
+    private static double decimalValue(String chainValue) {
+        int baseRedux = BASE-1;
+        double decimalValue = 0;
+
+        for (int i = 0; i <BASE; i++){
+            decimalValue = decimalValue + Integer.parseInt(chainValue.substring(i,i+1)) * Math.pow(BASE,baseRedux);
+            baseRedux--;
+        }
+        return decimalValue;
+    }
+
+    private static HashSet<String>  permutationPossibilities() {
+        HashSet<String> options = new HashSet<>();
+        try {
+            String sCurrentLine = reader.readLine();
+            StringBuilder valuesToPermutate = new StringBuilder();
+            BASE = sCurrentLine.length();
+            for (int i = 0; i < sCurrentLine.length(); i++) {
+                valuesToPermutate.append(i);
+            }
+            options = generatePerm(valuesToPermutate.toString());
+            removeInitializeZero(options);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return options;
+    }
+
+
+    private static void removeInitializeZero(Set<String> options) {
+        Iterator<String> iterator = options.iterator();
+        while (iterator.hasNext()) {
+            String setElement = iterator.next();
+            if (setElement.startsWith("0")) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static HashSet<String> generatePerm(String input) {
+        HashSet<String> set = new HashSet<String>();
+        if (input == "")
+            return set;
+
+        Character a = input.charAt(0);
+
+        if (input.length() > 1) {
+            input = input.substring(1);
+
+            Set<String> permSet = generatePerm(input);
+
+            for (String x : permSet) {
+                for (int i = 0; i <= x.length(); i++) {
+                    set.add(x.substring(0, i) + a + x.substring(i));
+                }
+            }
+        } else {
+            set.add(a + "");
+        }
+        return set;
+    }
+
+ }
